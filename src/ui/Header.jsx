b9aab85781818210464ddcfebf1ff13eda10ba7b
justin lovell <justinlvell18@gmail.com>
2024-10-21 15:01:45 +0700

    styling header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
+
+  const linkStyle = ({isActive}) => `w-24 py-2 transition-all duration-500 ${isActive ? "bg-gray-200 rounded-sm" : ""}`
+
   return (
-    <div>Header</div>
-  )
+    <nav className="white flex justify-center gap-5 border-b border-black py-5 items-center text-center">
+        <NavLink
+          to="/dashboard"
+          className={linkStyle}
+        >
+          Dashboard
+        </NavLink>
+
+      <NavLink to="/shop" className={linkStyle}>
+        Shop
+      </NavLink>
+    </nav>
+  );
 }
