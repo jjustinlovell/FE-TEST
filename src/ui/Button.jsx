3a1cc80d98693dc350954a10f3c258e159847466
justin lovell <justinlvell18@gmail.com>
2024-10-22 14:21:53 +0700

    deleting MenClothes & WomenClothes

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Button({ children, to, type, onClick, disabled }) {
   const base =
-    "px-7 py-2 rounded-md transition-all duration-500 border border-black ";
+    "px-7 py-2 rounded-md transition-all duration-500 border border-black";
 
   const styles = {
     black: base + " bg-black text-white ",
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
 
-  const linkStyle = ({isActive}) => `w-24 py-2 transition-all duration-500 ${isActive ? "bg-gray-200 rounded-sm" : ""}`
+  const linkStyle = ({isActive}) => `w-36 py-2 transition-all duration-500 ${isActive ? "bg-gray-200 rounded-sm text-gray-500" : ""}`
 
   return (
     <nav className="flex items-center justify-center gap-5 py-5 text-center white">
