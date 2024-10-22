126523d8d4f3691250c791108fb6c98499676cb0
justin lovell <justinlvell18@gmail.com>
2024-10-22 19:19:56 +0700

    adding more data into trendingproducts

--- src/data/TrendingProduct.js
+++ src/data/TrendingProduct.js
@@ -1,24 +1,42 @@
 const TrendingProduct = [
   {
-    path : '/assets/images/P1.avif',
-    name :  'Basic T-Shirt',
-    price : 'Rp. 100.000'
+    path: "/assets/images/P1.avif",
+    name: "Basic T-Shirt",
+    price: "Rp. 100.000",
   },
   {
-    path : '/assets/images/C1.avif',
-    name :  'Basic Women Shirt',
-    price : 'Rp. 100.000'
+    path: "/assets/images/C1.avif",
+    name: "Basic Women Shirt",
+    price: "Rp. 100.000",
   },
   {
-    path : '/assets/images/K1.avif',
-    name :  'Graphic Shirt',
-    price : 'Rp. 120.000',
+    path: "/assets/images/K1.avif",
+    name: "Graphic Shirt",
+    price: "Rp. 120.000",
   },
   {
-    path : '/assets/images/P2.avif',
-    name :  'Basic Shirt',
-    price : 'Rp. 100.000',
+    path: "/assets/images/P2.avif",
+    name: "Basic Shirt",
+    price: "Rp. 100.000",
   },
-]
+  {
+    path: "/assets/images/K2.avif",
+    category: "KIDS",
+    name: "Summer Shorts",
+    price: "Rp.99,000",
+  },
+  {
+    path: "/assets/images/K3.avif",
+    category: "KIDS",
+    name: "Hoodie",
+    price: "Rp.199,000",
+  },
+  {
+    path: "/assets/images/C1.avif",
+    category: "WOMEN",
+    name: "Summer Dress",
+    price: "Rp.299,000",
+  },
+];
 
 export default TrendingProduct;
